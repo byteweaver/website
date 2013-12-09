bddf7f4bbb272e6454a12fab9b6966e396184bf9
noxan <noxan@byteweaver.net>
2013-12-10 00:46:33 +0100

    write simple angular controller just containing a list of projects

---
+++ controllers.js
@@ -0,0 +1,13 @@
+function ProjectCtrl($scope) {
+  $scope.projects = [
+    {'name': 'django-posts'},
+    {'name': 'django-forums'},
+    {'name': 'django-referral'},
+    {'name': 'django-coupons'},
+    {'name': 'django-tickets'},
+    {'name': 'django-skrill'},
+    {'name': 'django-polls'},
+    {'name': 'django-singleactiveobject'},
+    {'name': 'django-eca-catalogue'}
+  ];
+};
