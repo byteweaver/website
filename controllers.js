8fddc683504d6cd692af9913c3b543a3637d551b
Richard <richard@devisio.net>
2015-06-07 12:02:28 +0200

    add blacklist to ignore django-tos project

--- controllers.js
+++ controllers.js
@@ -1,7 +1,9 @@
 function ProjectCtrl($scope, $http) {
+  var blacklist = ['django-tos'];
+
   $http.get('https://api.github.com/orgs/byteweaver/repos').success(function (repos) {
     $scope.projects = repos.filter(function (repo) {
-      return repo.name.startsWith('django-');
+      return repo.name.startsWith('django-') && blacklist.indexOf(repo.name) === -1;
     });
   });
 };
